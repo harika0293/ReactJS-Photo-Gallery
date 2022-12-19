a22bd4751ad3e403ec7bb452844e5b52be116476
Harika Rachamalla <harika.0293@gmail.com>
2022-12-19 18:48:20 +0530

    Updates

---
+++ precache-manifest.3bc5546a40d83296047b805fdc643d5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4144f52a2fdd76c29d5f710b3ade2df",
+    "url": "/ReactJS-Photo-Gallery/index.html"
+  },
+  {
+    "revision": "8f381145c1ca64a16494",
+    "url": "/ReactJS-Photo-Gallery/static/css/main.ec41352a.chunk.css"
+  },
+  {
+    "revision": "0120793404275b0d1d53",
+    "url": "/ReactJS-Photo-Gallery/static/js/2.7883686a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ReactJS-Photo-Gallery/static/js/2.7883686a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f381145c1ca64a16494",
+    "url": "/ReactJS-Photo-Gallery/static/js/main.387f435f.chunk.js"
+  },
+  {
+    "revision": "776f0ddc8e70484db23f",
+    "url": "/ReactJS-Photo-Gallery/static/js/runtime-main.69ff59dd.js"
+  }
+]);
